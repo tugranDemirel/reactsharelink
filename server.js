34d475328be886fcb28f970a43a32bf0d9c4cf99
Tuğran Demirel <49235236+tugranDemirel@users.noreply.github.com>
2023-11-22 17:34:52 +0300

    wip

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
-var app = require('express')();
-var http = require('http').Server(app());
-var io = require('socket.io')(http);
-var port = process.env.REACT_APP_SOCKET_PORT;
+var app = require('express')()
 
-app.get('/', function (req, res){
-    res.send('Test İstek');
+var http = require('http').Server(app)
+
+var io = require('socket.io')(http)
+
+var port = process.env.REACT_APP_SOCKET_PORT || 3001
+
+app.get('/', function (request, response){
+    response.send('Test İstek')
+})
+
+io.on("connection", (socket) => {
+    console.log('bağlantı sağlandı')
+
+    socket.on('send_data', (data) => {
+        socket.broadcast.emit('push_data', {
+            url: data.url,
+        })
+       /* socket.emit('welcome', {
+            text: `Sitede bir link paylaşıldı: ${data.url}`,
+        })*/
+    })
+});
+
+
+http.listen(port, function () {
+    console.log(`server is running: http://localhost:${port}`)
 })
--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import './App.css';
-
+import SocketIO from 'socket.io-client'
+import {useEffect, useState} from "react";
+const socket = SocketIO("http://localhost:3001", {
+    transports: ['websocket', 'polling', 'flashsocket']
+})
 function App() {
+    const [text, setText] = useState('')
+    const  [data, setData] = useState([])
+    useEffect(() => {
+        socket.on('push_data', (res) => {
+            data.push(res.url)
+            setData([...data])
+        })
+    }, []);
+    const sendData = () => {
+        if (text != '')
+        {
+            socket.emit('send_data', {
+                url: text,
+            })
+            data.push(text)
+            setData([...data])
+            setText('')
+        }
+        else
+            alert('Lütfen Link Giriniz.')
+    }
   return (
-    <div className="App">
-      hello
+    <div >
+        <div style={{ marginTop:10, justifyContent: 'center', display:'flex', alignItems:'center' }}>
+            <input style={{ padding:10, width:300, borderRadius: 5, border: '1px solid #a5a5a5' }} type="text" onChange={(event) => setText(event.target.value)} value={text} placeholder="Paylaşmak istediğiniz link"/>
+            <button style={{ padding: '7px 15px', border: '1px solid #ddd', borderRadius: 5, marginLeft: 5 }} onClick={sendData}>Veriyi Gönder</button>
+        </div>
+        <div className="link-container">
+            { data.map((item) => (
+                <div className="link-item">
+                    <a href={item} target="_blank">{item}</a>
+                </div>
+            ))}
+        </div>
     </div>
   );
 }
